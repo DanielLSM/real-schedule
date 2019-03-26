a4f8994566a841900303d0501ab314f2fd619ca4
daniel <daniellsmarta@gmail.com>
2019-03-26 17:37:55 +0100

    parser changed to make ordered dict

--- reals/core/parser.py
+++ reals/core/parser.py
@@ -50,30 +50,36 @@ def book_to_calendar_kwargs_qichen(book: dict):
 def book_to_kwargs_MPO(book):
     """ given an MPO input, compute dict where keys are aircraft ids and the rest of sheet info is
     organized by aircraft id """
-    aircraft_info = get_aircraft_info(book)
-    calendar_restrictions = get_calendar_restrictions(book)
+    aircraft_info = get_aircraft_info_MPO(book)
+    calendar_restrictions = get_calendar_restrictions_MPO(book)
     
     return aircraft_info, calendar_restrictions
 
+def get_calendar_restrictions_MPO(book):
+    pass
+
 def get_aircraft_info_MPO(book):
+    print('INFO: gathering aircraft info')
+    
     aircraft_info = OrderedDict()    
     for sheet_name in book.keys():
         if 'Aircraft ID' in book[sheet_name].keys():
             #create ordered dict to store aircraft info
-            for a_id in range(len(book[sheet_name]['Aircraft ID'])):
+            for _ in range(len(book[sheet_name]['Aircraft ID'])):
                 a_id = book[sheet_name]['Aircraft ID'][_]
-                if a_id is not in aircraft_info.keys():
+                if a_id not in list(aircraft_info.keys()):
                     aircraft_info[a_id] = OrderedDict()
-                if sheet_name is not in aircraft_info[a_id].keys():
+                if sheet_name not in list(aircraft_info[a_id].keys()):
                     aircraft_info[a_id][sheet_name] = OrderedDict()
 
-            #fill the info of other columns
+            #fill the info of other columns, pandas already adds idx to equal value columns
             for column_idx in book[sheet_name].keys():
-                if column_idx is not 'Aircraft ID': 
+                if column_idx != 'Aircraft ID':
                     for _ in range(len(book[sheet_name]['Aircraft ID'])):
                         a_id = book[sheet_name]['Aircraft ID'][_]
                         aircraft_info[a_id][sheet_name][column_idx] = book[sheet_name][column_idx][_]
     
+    print('INFO: aircraft info completed')
     return aircraft_info
     
 
@@ -101,10 +107,10 @@ def to_excel():
 if __name__ == '__main__':
     try:
         book = excel_to_book(f1_in)
-        print(book)
     except Exception as e:
         print('you messed up')
         raise e
-    book_to_kwargs_MPO(book)
+    aircraft_info = get_aircraft_info_MPO(book)
+    # book_to_kwargs_MPO(book)
     # print("congratulations buddy!!!")
     # book_to_calendar_kwargs_qichen(book)
