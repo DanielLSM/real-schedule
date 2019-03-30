5579bc8d83521ba9a97f172733b0be3d760b88d9
daniel <daniellsmarta@gmail.com>
2019-03-31 00:06:13 +0100

    parser polishing

--- reals/core/parser.py
+++ reals/core/parser.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import random
 from reals.core.resources import f1_in, f2_out
-from reals.core.utils import advance_date, advance_date_now, dict_to_list, diff_time_list, get_slots
+from reals.core.utils import dict_to_list, diff_time_list, get_slots
 
 from collections import OrderedDict, defaultdict
 
@@ -10,11 +10,13 @@ from collections import OrderedDict, defaultdict
 
 def excel_to_book(file_input: str):
     try:
-        book = pd.read_excel(file_input, sheet_name=None)  # returns an ordered dict
+        book = pd.read_excel(
+            file_input, sheet_name=None)  # returns an ordered dict
     except Exception as e:
         print(e)
         print('Error parsing the excel file into a dict book buddy!')
     return book
+
 
 def book_to_kwargs_MPO(book):
     print("INFO: building from xlsx")
@@ -23,32 +25,36 @@ def book_to_kwargs_MPO(book):
     aircraft_info = get_aircraft_info_MPO(book)
     calendar_restrictions = get_restrictions_MPO(book)
 
-
     # each type of maintenance as several restrictions we will devide in 2
     # time and hangar restrictions
     m_type_restriction = {}
-    m_type_restriction = {'time_type':'day'}
-    
+    m_type_restriction = {'time_type': 'day'}
+
     a_time = dict_to_list(calendar_restrictions['A_Not_Allowed']['Dates'])
     c_time = diff_time_list(calendar_restrictions['C_Not_Allowed'])
     all_time = dict_to_list(calendar_restrictions['Public_Holidays']['Dates'])
 
+    a_resources = {
+        'resources': {
+            'extra_slots': get_slots(calendar_restrictions['More_A_Slots'])
+        }
+    }
+    c_resources = {
+        'resources': {
+            'extra_slots': get_slots(calendar_restrictions['More_C_Slots'])
+        }
+    }
 
-    a_resources = {'resources':{'extra_slots':get_slots(calendar_restrictions['More_A_Slots'])}}
-    c_resources = {'resources':{'extra_slots':get_slots(calendar_restrictions['More_C_Slots'])}}
+    m_type_restriction['a-type'] = {'time': a_time, 'resources': a_resources}
+    m_type_restriction['c-type'] = {'time': c_time, 'resources': c_resources}
+    m_type_restriction['all'] = {'time': all_time}
 
-
-    m_type_restriction['a-type']= {'time':a_time, 'resources':a_resources}
-    m_type_restriction['c-type']= {'time':c_time, 'resources':c_resources}
-    m_type_restriction['all']= {'time': all_time}
-
-    
+    # TODO
+    # start_date =
+    # end_date  =
 
     # all these restrictions will restrict the general calendar
-    # for 
-
-    import ipdb;
-    ipdb.set_trace();
+    # for
     print("INFO: information from xlsx parsed with success")
     return {'aircraft_info': aircraft_info, 'restrictions': m_type_restriction}
 
@@ -58,6 +64,7 @@ def book_to_kwargs_MPO(book):
     # calendar_kwargs['total_years'] = additional['Total Years'][2017]
 
     # calendar = Calendar(**calendar_kwargs)
+
 
 def get_restrictions_MPO(book):
 
@@ -71,13 +78,14 @@ def get_restrictions_MPO(book):
     print('INFO: restrictions info completed')
     return restrictions_info
 
+
 def get_aircraft_info_MPO(book):
     print('INFO: gathering aircraft info')
-    
-    aircraft_info = OrderedDict()    
+
+    aircraft_info = OrderedDict()
     for sheet_name in book.keys():
         if 'Aircraft ID' in book[sheet_name].keys():
-            #create ordered dict to store aircraft info
+            # create ordered dict to store aircraft info
             for _ in range(len(book[sheet_name]['Aircraft ID'])):
                 a_id = book[sheet_name]['Aircraft ID'][_]
                 if a_id not in list(aircraft_info.keys()):
@@ -85,22 +93,25 @@ def get_aircraft_info_MPO(book):
                 if sheet_name not in list(aircraft_info[a_id].keys()):
                     aircraft_info[a_id][sheet_name] = OrderedDict()
 
-            #fill the info of other columns, pandas already adds idx to equal value columns
+            # fill the info of other columns, pandas already adds idx to equal
+            # value columns
             for column_idx in book[sheet_name].keys():
                 if column_idx != 'Aircraft ID':
                     for _ in range(len(book[sheet_name]['Aircraft ID'])):
                         a_id = book[sheet_name]['Aircraft ID'][_]
-                        aircraft_info[a_id][sheet_name][column_idx] = book[sheet_name][column_idx][_]
-    
+                        aircraft_info[a_id][sheet_name][column_idx] = book[
+                            sheet_name][column_idx][_]
+
     print('INFO: aircraft info completed')
     return aircraft_info
-    
+
 
 def book_to_tasks(book: dict):
     pass
 
+
 def to_excel():
-    #will save something to an excel eventually
+    # will save something to an excel eventually
     pass
 
 
@@ -112,14 +123,6 @@ if __name__ == '__main__':
         raise e
 
     kwargs = book_to_kwargs_MPO(book)
-
-
-
-
-
-
-
-
 
     # book_to_kwargs_MPO(book)
     # print("congratulations buddy!!!")
--- reals/core/schedule_classes.py
+++ reals/core/schedule_classes.py
@@ -2,15 +2,17 @@ import numpy
 import pandas as pd
 from collections import OrderedDict, defaultdict
 # from parser import book_to_kwargs_MPO
+
+from reals.core.resources import f1_in, f2_out
 from reals.core.parser import book_to_kwargs_MPO
 
-class Calendar:
 
+class Calendar:
     def __init__(self, *args, **kwargs):
         self.start_date = kwargs['start_date']
         self.end_date = kwargs['end_date']
         self.total_years = kwargs['total_years']
-        #all the not allowed days
+        # all the not allowed days
         self.not_allowed = kwargs['not_allowed']
         self.calendar = self._setup_calendar()
 
@@ -30,7 +32,7 @@ class Calendar:
         pass
 
     def render(self):
-        pass  #something something tkinter?
+        pass  # something something tkinter?
 
     def _setup_calendar(self):
         calendar = OrderedDict()
@@ -40,16 +42,15 @@ class Calendar:
 
 
 class Fleet:
-
     def __init__(self, *args, **kwargs):
         self.name = 0
 
         self.C_previous_code = None
-        self.C_max = None  #total numbers of c check in  c check cycle
+        self.C_max = None  # numbers of c check in  c check cycle
         # actual variables
-        self.DY_C = 0  #callendary days since previous C check
-        self.FH_C = 0  #flight hours since previous C check
-        self.FC_C = 0  #flight hours since previous C check
+        self.DY_C = 0  # callendary days since previous C check
+        self.FH_C = 0  # hours since previous C check
+        self.FC_C = 0  # hours since previous C check
 
         # Intervals
         self.DY_C_interval = 0
@@ -58,3 +59,21 @@ class Fleet:
 
         self.C_previous_start = 0
         self.C_previous_end = 0
+
+
+# this should be an abc abstract class
+class FleetManagerBase:
+    def __init__(self, *args, **kwargs):
+        self.fleet = Fleet(**kwargs['aircraft_info'])
+        self.calendar = Calendar(**kwargs['calendar_restrictions'])
+
+
+if __name__ == '__main__':
+    from reals.core.parser import excel_to_book
+    try:
+        book = excel_to_book(f1_in)
+    except Exception as e:
+        print('you messed up')
+        raise e
+
+    kwargs = book_to_kwargs_MPO(book)
