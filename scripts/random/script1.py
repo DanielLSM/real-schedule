ea3610354e60678fb9243ef83e66de3f739ffe1e
daniel <daniellsmarta@gmail.com>
2019-04-28 02:09:22 +0200

    good idea

---
+++ scripts/random/script1.py
@@ -0,0 +1,8 @@
+# The idea to implement here is simple. We are gonna implement
+# branch and bound with RL, for now lets implement branch and bound
+# we need a way to formulate MILPS on the fly, compare with general MILP
+# fit checks on/tasks on due dates, thats it. variables are
+# maintenance opportunity by time.
+# resctriction is due date, objective function is?
+# minimize tardiness, with equal costs? yes
+# okay
